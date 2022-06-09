4ebb5e4c3aae5d51bc65850108985eb9904b1aa4
priti <roypriti921@gmail.com>
2022-06-09 13:49:23 +0530

    added components and connected sanity client with front-end, working on adding styles

---
+++ components/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {urlFor} from '../lib/client'
+
+const Product = ({ product: {image, name,slug, price} }) => {
+  return (
+    <div>product</div>
+    
+  )
+}
+
+export default Product
